39aab43059267a59ebafa0ed4ba7854f93893619
demiren <demiren@localhost>
2022-10-12 15:42:08 -0500

    hi

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,63 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeName = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		StdDraw.setPenColor(red, green, blue);
+				
+		boolean isFilled = in.nextBoolean();
+		
+		
+		if (shapeName.equals("rectangle")||shapeName.equals("ellipse"))
+		{
+			double w = in.nextDouble();
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double z = in.nextDouble();
+			
+			if (shapeName.equals("rectangle")) {
+				if (isFilled) {
+					StdDraw.filledRectangle(w, x, y, z);
+					
+				} else {
+					StdDraw.rectangle(w, x, y, z);
+				}	
+			}
+			
+
+			if (shapeName.equals("ellipse")) {
+				if (isFilled) {
+					StdDraw.filledEllipse(w, x, y, z);
+					
+				} else {
+					StdDraw.ellipse(w, x, y, z);
+				}	
+			}
+			
+		}
+		else if (shapeName.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			
+			double [] x = {x1, x2, x3};
+			double [] y = {y1, y2, y3};
+			
+			if (isFilled) {
+				StdDraw.filledPolygon(x, y);
+				
+			} else {
+				StdDraw.polygon(x, y);
+			}	
+}
+		
+
+			
+		
 	}
 }
